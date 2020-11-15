abfe9eab40fb0cb2cbf9e0c2d01c50a54b1f8a2d
MathPelicer <matheuspelicer58@gmail.com>
2020-11-15 20:42:13 -0300

    addNodes HASH

--- visHASH.js
+++ visHASH.js
@@ -82,6 +82,7 @@ function Add(){
     var value = document.getElementById("inpValue").value;
     let kv = new KeyValue(key, value);
     hd.insert(kv);
+    AddNodes(key, value);
 }
 
 function Delete(){
@@ -201,11 +202,11 @@ function StartingNodes(){
     });
 
     for(var i = 0; i < MAX; i++){
-        var str = "Index " + i;
+        var str = "" + i;
         nodes.add({
             id: nodesIds,
             label: str,
-            group: 3,
+            group: i,
         });
         nodesIds++;
     }
@@ -222,142 +223,75 @@ function StartingNodes(){
 }
 
 function AddNodes() {
-    newNodeValue = document.getElementById("inpValue").value;
-    l.insert(newNodeValue);
+    var newNodeValue = document.getElementById("inpValue").value;
+    var newNodeKey = document.getElementById("keyValue").value;
+    var index = hd.hashFunc(newNodeKey % MAX);
+
+    console.log("Chave: " + newNodeKey)
+    console.log("Valor: " + newNodeValue)
 
     var nodesItems = nodes.get({
-        fields: ['id', 'label'],
-        type: {
-            date: 'ISODate'
-        }
+        fields: ['id', 'label', 'group'],
     });
  
     var edgesItems = edges.get({
         fields: ['id', 'from', 'to'],
-        type: {
-            date: 'ISODate'
-        }
     });
 
-    // criar função separada pro bobble sort
+    console.log(hd.table)   
+    
+    if(hd.table[index].length <= 1){
+        nodes.add({
+            id: nodesIds,
+            label: newNodeValue,
+            group: index,
+        });
 
-    for(var j = 0; j < nodesItems.length; j++){
-        for(var i = 0; i < nodesItems.length - 1; i++){
-            if(parseInt(nodesItems[i].label) > parseInt(nodesItems[i + 1].label)){
-                var temp = nodesItems[i];
-                nodesItems[i] = nodesItems[i + 1];
-                nodesItems[i + 1] = temp;
-            }
-        }
+        edges.add({
+            id: edgesIds,
+            from: index,
+            to: nodesIds,
+        })
+
+        nodesIds++;
+        edgesIds++;
     }
+    else{
+        var lastElementIndex = hd.table[index].length - 2;
 
-    console.log(nodesItems);
+        console.log(lastElementIndex)
 
-    var smaller;
-    var greater;
+        for(var i = 0; i < nodes.length; i ++){
+            var curNode = nodesItems[i];
 
-    for (var i = 0; i < nodes.length; i++) {
-        index = nodesItems[i];
-
-        if (parseInt(newNodeValue) < parseInt(index.label)) {
-
-            if (!smaller) {
-                smaller = index.id;
-                smallerValue = parseInt(index.label);
-            } else if (parseInt(index.label) < smallerValue) {
-                smaller = index.id;
-            }
-
-        } else if (index.label != "") {
-
-            if (!greater) {
-                greater = index.id;
-                greaterValue = parseInt(index.label);
-            } else if (parseInt(index.label) >= greaterValue) {
-                greater = index.id;
-                greaterValue = parseInt(index.label)
-            }
-        }
-    }
-
-    console.log("greater: " + greater)
-    console.log("smaller: " + smaller)
-
-    nodes.add({
-        id: nodesIds,
-        label: newNodeValue,
-        group: 0
-    });
-    var id = nodesIds;
-    nodesIds++;
-
-    for (var i = 0; i < edgesItems.length; i++) {
-        if (smaller && greater) {
-            if ((edgesItems[i].from == smaller && edgesItems[i].to == greater) ||
-                (edgesItems[i].to == smaller && edgesItems[i].from == greater)) {
-                console.log("Removed: " + edgesItems[i])
-                edges.remove({
-                    id: edgesItems[i].id
+            console.log(curNode)
+            console.log(hd.table[index][lastElementIndex].value)
+            
+            if(curNode.label == hd.table[index][lastElementIndex].value){
+                console.log("achei")
+                nodes.add({
+                    id: nodesIds,
+                    label: newNodeValue,
+                    group: index,
                 });
-            }
-        }
-        if (!smaller) {
-            if (edgesItems[i].to == '1') {
-                edges.remove({
-                    id: edgesItems[i].id
-                });
-                edgesIds++;
+        
                 edges.add({
                     id: edgesIds,
-                    from: id,
-                    to: '1'
+                    from: curNode.id,
+                    to: nodesIds,
                 });
-            }
-        }
-        if (!greater) {
-            if (edgesItems[i].to == '0') {
-                edges.remove({
-                    id: edgesItems[i].id
-                });
+                
+                nodesIds++;
                 edgesIds++;
-                edges.add({
-                    id: edgesIds,
-                    from: id,
-                    to: '0'
-                });
             }
         }
     }
-
-    edgesIds++;
-    edges.add({
-        id: edgesIds,
-        from: smaller,
-        to: id
-    })
-    edgesIds++;
-    edges.add({
-        id: edgesIds,
-        from: id,
-        to: smaller
-    })
-    edgesIds++;
-    edges.add({
-        id: edgesIds,
-        from: greater,
-        to: id
-    })
-    edgesIds++;
-    edges.add({
-        id: edgesIds,
-        from: id,
-        to: greater
-    })
 }
 
 function RemoveNode(){
     var i;
     var removedNodeValue = document.getElementById("inpValue").value;
+    var removedNodeValue = document.getElementById("keyValue").value;
 
     var verify = l.remove(parseInt(removedNodeValue));
 
@@ -373,125 +307,7 @@ function RemoveNode(){
         fields: ['id', 'from', 'to']
     });
 
-    for(var j = 0; j < nodesItems.length; j++){
-        for(i = 0; i < nodesItems.length - 1; i++){
-            if(parseInt(nodesItems[i].label) > parseInt(nodesItems[i + 1].label)){
-                var temp = nodesItems[i];
-                nodesItems[i] = nodesItems[i + 1];
-                nodesItems[i + 1] = temp;
-            }
-        }
-    }
-
-    console.log(removedNodeValue);
-
-    var prevNodeId;
-    var nextNodeId;
-    var removedNodeId;
-    var firstNode;
-    var lastNode;
-
-    /*while(parseInt(nodesItems[i].label) < parseInt(removedNodeValue) && i + 1 < nodesItems.length){
-        prevNode = nodesItems[i];
-        curNode = nodesItems[i + 1];
-        i++;
-    }
-
-    if(curNode.data != parseInt(removedNodeValue)){
-        return false;
-    }
-
-    if()*/
-
-    console.log(nodesItems);
-    console.log(edgesItems);
-
-    for(i = 2; i < nodes.length; i++){
-        console.log("Iterações: ", i-1);
-        console.log(parseInt(nodesItems[i].label));
-        if(parseInt(nodesItems[i].label) == removedNodeValue){
-            console.log("achou o valor");
-            removedNodeId = nodesItems[i].id;
-            nodes.remove({id: removedNodeId});
-            console.log("removeu o número");
-            /*prevNodeId = nodesItems[i-1].id;
-            nextNodeId = nodesItems[i+1].id;*/
-            if(i == 2){
-                console.log("primeiro node");
-                firstNode = true;
-                prevNodeId = nodesItems[0].id;
-                nextNodeId = nodesItems[i+1].id;
-                break;
-            }
-            else if(i == nodes.length){
-                console.log("ultimo node");
-                lastNode = true;
-                prevNodeId = nodesItems[i-1].id;
-                break;
-            }
-            else{
-                console.log("outro node");
-                prevNodeId = nodesItems[i-1].id;
-                nextNodeId = nodesItems[i+1].id;
-                break;
-            }
-        }
-    }
-
-    //console.log("estou aqui");
-    /*console.log("Prev: ", prevNodeId);
-    console.log("Next: ", nextNodeId);
-
-    console.log("Id: 0 = ", nodesItems[0]);
-    console.log("Id: 1 = ", nodesItems[1]);*/
-
-    for(i = 0; i < edges.length; i++){
-        if(edgesItems[i].from == removedNodeId){
-            edges.remove({id: edgesItems[i].id});
-        }
-
-        /*if(firstNode){
-            if(edgesItems[i].from == nextNodeId && edgesItems[i].to == removedNodeId){
-                console.log(edgesItems[i]);
-                edges.update({id: edgesItems[i].id, to: "0"});
-            }
-
-        }
-        else if(lastNode){
-            if(edgesItems[i].from == prevNodeId && edgesItems[i].to == removedNodeId){
-                console.log(edgesItems[i]);
-                edges.update({id: edgesItems[i].id, to: "1"});
-            }
-
-        }
-        else{
-            if(edgesItems[i].from == prevNodeId && edgesItems[i].to == removedNodeId){
-                console.log(edgesItems[i]);
-                edges.update({id: edgesItems[i].id, to:nextNodeId});
-            }
-            else if(edgesItems[i].from == nextNodeId && edgesItems[i].to == removedNodeId){
-                console.log(edgesItems[i]);
-                edges.update({id: edgesItems[i].id, to:prevNodeId});
-            }*/
-
-        if(edgesItems[i].from == prevNodeId && edgesItems[i].to == removedNodeId){
-                //console.log(edgesItems[i]);
-                edges.update({id: edgesItems[i].id, to:nextNodeId});
-        }
-        else if(edgesItems[i].from == nextNodeId && edgesItems[i].to == removedNodeId){
-                //console.log(edgesItems[i]);
-                edges.update({id: edgesItems[i].id, to: prevNodeId});
-        }
-        
-    }
-
-    if(lastNode){
-        edges.update({id: edgesItems[edges.length].id, from: prevNodeId, to: 1});
-    }
-
-    console.log(edgesItems[edges.length]);
-
-    return true;
+    
 
 }
 
